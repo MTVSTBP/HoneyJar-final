cbceaf3423d0d3e7ea9a5a002b57a1a6a72e4f5a
LooanCheong <jjyejun@naver.com>
2024-07-21 12:53:29 +0900

    Feat: 어드민 Qna 메인페이지 Get 매핑

--- src/main/java/com/tbp/honeyjar/admin/dao/QnaMapper.java
+++ src/main/java/com/tbp/honeyjar/admin/dao/QnaMapper.java
@@ -1,7 +1,12 @@
 package com.tbp.honeyjar.admin.dao;
 
+import com.tbp.honeyjar.admin.dto.qna.QnaListResponseDto;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface QnaMapper {
+    
+    List<QnaListResponseDto> findAllQna();
 }
