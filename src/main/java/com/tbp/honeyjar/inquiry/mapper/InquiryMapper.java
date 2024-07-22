4db92eb6ea7dca60d05a9f11d4b7a7a7a06d056d
junsu <junsyun22@gmail.com>
2024-07-22 13:08:36 +0900

    Feat: 1:1문의 Mapper #1 문의 목록 getInquiryList(); #2 문의 상세 getInquiryById(@Param("id") Long id); #3 문의 작성 create();
    #4 문의 수정, 삭제
    create();
    delete();

--- src/main/java/com/tbp/honeyjar/inquiry/mapper/InquiryMapper.java
+++ src/main/java/com/tbp/honeyjar/inquiry/mapper/InquiryMapper.java
@@ -1,13 +1,24 @@
 package com.tbp.honeyjar.inquiry.mapper;
 
 import com.tbp.honeyjar.inquiry.dto.InquiryDto;
+import com.tbp.honeyjar.inquiry.dto.InquiryUpdateDto;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 @Mapper
 public interface InquiryMapper {
+    @Select("SELECT * FROM inquiry")
     List<InquiryDto> getInquiryList();
-    int create(InquiryDto inquiryDto);
-}
 
+    @Select("SELECT * FROM inquiry WHERE inquiry_id = #{id}")
+    InquiryDto getInquiryById(@Param("id") Long id);
+
+    int create(InquiryDto inquiryDto);
+
+    int update(InquiryUpdateDto inquiryUpdateDto);
+
+    int delete(Long id);
+}
