30c541da5623f3ba2e63293ad64d7ead22718650
LooanCheong <jjyejun@naver.com>
2024-07-22 19:56:57 +0900

    Feat: 어드민 카테고리 ID 기반 단일 조회 쿼리 매핑

--- src/main/java/com/tbp/honeyjar/admin/dao/QnaMapper.java
+++ src/main/java/com/tbp/honeyjar/admin/dao/QnaMapper.java
@@ -1,12 +1,15 @@
 package com.tbp.honeyjar.admin.dao;
 
 import com.tbp.honeyjar.admin.dto.qna.QnaListResponseDto;
+import com.tbp.honeyjar.admin.dto.qna.QnaResponseDto;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 @Mapper
 public interface QnaMapper {
-    
+
     List<QnaListResponseDto> findAllQna();
+
+    QnaResponseDto findById(Long id);
 }
