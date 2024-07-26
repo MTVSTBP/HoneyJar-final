340e712cc180a66fc2ff353556414699c5a6fdfe
lsy1307 <lsy1307@inha.edu>
2024-07-26 12:57:57 +0900

    Feat: 흠

--- src/main/java/com/tbp/honeyjar/follow/controller/FollowController.java
+++ src/main/java/com/tbp/honeyjar/follow/controller/FollowController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
-//
+
 @Controller
 @RequestMapping("/follow")
 public class FollowController {
