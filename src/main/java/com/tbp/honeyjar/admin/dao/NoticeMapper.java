531231456317d0c5592987f39a8a4d115e9bbfff
junsyun22 <junsyun22@gmail.com>
2024-07-28 21:25:04 +0900

    Feat/notice 어드민 공지 조회 페이지네이션 (#64)
    * save
    
    * Add: pagenation.js 분리
    
    * Fix:
    NoticeMapper.xml
    - Added getNoticeCount sql
    - adminNoticeListResultMap added Column (updatedAt)
    
    - findAllNotice sql do select from notice with paging-infomation (offset, pagesize)
    
    * Feat: Notice Service
    # findAllNotices Method
    setting paging-info(offset,pageSize) into Map-Object "params".
    
    * Feat: findAllNotice needs Map-Object "params" for sql's parameter.
    
    * Fix: admin notice pagenation

--- src/main/java/com/tbp/honeyjar/admin/dao/NoticeMapper.java
+++ src/main/java/com/tbp/honeyjar/admin/dao/NoticeMapper.java
@@ -5,13 +5,17 @@ import com.tbp.honeyjar.admin.dto.notice.NoticeListResponseDto;
 import com.tbp.honeyjar.admin.dto.notice.NoticeResponseDto;
 import com.tbp.honeyjar.admin.dto.notice.NoticeSaveRequestDto;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface NoticeMapper {
 
-    List<NoticeListResponseDto> findAllNotices();
+//    List<NoticeListResponseDto> findAllNotices();
+
+    List<NoticeListResponseDto> findAllNotices(@Param("params") Map<String, Object> params);
 
     NoticeResponseDto findById(Long id);
 
@@ -20,4 +24,6 @@ public interface NoticeMapper {
     Long correction(NoticeCorrectionRequestDto dto);
 
     Long delete(Long postId);
+
+    int getNoticeCount();
 }
