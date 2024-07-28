6e5dbc7594d454218b7bf85cfdcec650d2adecb3
bellrajin <parkjin931@gmail.com>
2024-07-28 21:51:31 +0900

    feat : 팔로우 기능 구현 진행중

--- src/main/java/com/tbp/honeyjar/follow/controller/FollowController.java
+++ src/main/java/com/tbp/honeyjar/follow/controller/FollowController.java
@@ -1,15 +1,17 @@
 package com.tbp.honeyjar.follow.controller;
 
 import com.tbp.honeyjar.follow.DTO.FollowerDTO;
+import com.tbp.honeyjar.follow.DTO.FollowingDTO;
 import com.tbp.honeyjar.follow.service.FollowService;
+import com.tbp.honeyjar.login.DTO.UserDTO;
 import com.tbp.honeyjar.login.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 @RequestMapping("/follow")
@@ -27,5 +29,40 @@ public class FollowController {
         FollowerDTO followerDTO = followService.getFollower(userId);
         model.addAttribute("follows", followerDTO);
         return "pages/follow/follower";
+    }
+
+    // 추가
+    @GetMapping("/following")
+    public String following(Model model, Principal principal) {
+        Long userId = userService.findUserIdByKakaoId(principal.getName());
+        FollowingDTO followingDTO = followService.getFollowing(userId);
+        model.addAttribute("followings", followingDTO);
+        return "pages/follow/following";
+    }
+
+    @PostMapping("/follow/{followUserId}")
+    public String followUser(@PathVariable Long followUserId, Principal principal) {
+        Long userId = userService.findUserIdByKakaoId(principal.getName());
+        followService.followUser(userId, followUserId);
+        return "redirect:/follow/following";
+    }
+
+    @PostMapping("/unfollow/{followUserId}")
+    public String unfollowUser(@PathVariable Long followUserId, Principal principal) {
+        Long userId = userService.findUserIdByKakaoId(principal.getName());
+        followService.unfollowUser(userId, followUserId);
+        return "redirect:/follow/following";
+    }
+
+    @GetMapping("/search")
+    public String searchUsers(@RequestParam(name = "name", required = false, defaultValue = "") String name, Model model, Principal principal) {
+        Long userId = userService.findUserIdByKakaoId(principal.getName());
+        List<UserDTO> users = userService.searchUsersByName(name);
+        for (UserDTO user : users) {
+            boolean isFollowed = followService.isFollowing(userId, user.getUserId());
+            user.setFollowed(isFollowed); // 오류 해결: UserDTO에 setFollowed 메서드 추가
+        }
+        model.addAttribute("users", users);
+        return "pages/follow/search";
     }
 }
