99bd1f7573f0bfe4cc2d5c4a59d1d96db393dac3
DVLP-Seok <dookie289@gmail.com>
2024-07-29 09:48:32 +0900

    1:1 문의 댓글 등록 구현 완료.

--- src/main/java/com/tbp/honeyjar/inquiry/dto/InquiryCommentDTO.java
+++ src/main/java/com/tbp/honeyjar/inquiry/dto/InquiryCommentDTO.java
@@ -1,10 +1,14 @@
 package com.tbp.honeyjar.inquiry.dto;
 
-import lombok.Data;
+import lombok.*;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-@Data
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 public class InquiryCommentDTO {
     private Long commentId;
     private String comment;
@@ -12,4 +16,16 @@ public class InquiryCommentDTO {
     private LocalDateTime updatedAt;
     private Long inquiryId;
     private Long userId;
+    private String name;
+
+    @Getter
+    private String formattedCreatedAt;
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+        if (createdAt != null) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy년 M월 d일 HH시 mm분");
+            this.formattedCreatedAt = createdAt.format(formatter);
+        }
+    }
 }
